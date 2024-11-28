fa45c144313d8d33c065065644bcf6302253f2a3
tomasz przechlewski <rudolf.von.ems@gmail.com>
2024-11-28 17:34:19 +0100

    ak

--- survey.R
+++ survey.R
@@ -1,19 +1,22 @@
+# -- Sousse 2024 --
 library("tidyverse")
 library("knitr")
 library("ggplot2")
 library("googlesheets4")
 
 ## Attitudes towards statistics survey (Sousse 2024)
+## r :: reverse questions
 ## ================================================
 question.names <- c("p1", "p2r", 'p3r', 'p4', 'p5r', 'p6r', 'p7',
-            'p8', 'p9r', "p10r", "p11r", 'p12r', 'p13', 'p14r', 'p15', 'p16r', 'p17',
+            'p8', 'p9r', "p10r", "p11r", 'p12r', 'p13', 'p14r', 
+            'p15', 'p16r', 'p17',
             'p18r', 'p19r', "p20r", "p21r", 'p22r', 'p23', 'p24', 'p25r', 'p26r', 'p27r', 'p28r',
             'age', 'sex')
 ##
 q.names <- c('time', question.names)
 
-## badaniu podlega tylko
-##
+## Starting page: https://tprzechlewski.blogspot.com/p/erasmus-2024.html
+## #############
 ## s01 <- read_sheet('1Njp2kcaLTB6DZuhyRX2yji67rwwAzhDDeZ0hf8jcNe4' )
 
 ## Link to survey form
@@ -51,15 +54,81 @@ s0 <- read_sheet('1RPn3sN6jCxmnYjTUF4Z9vkM6GNrGjQI9v6fkbGDei4U', skip = 1, col_n
   mutate (pws = p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10 +
             p11 + p12 + p13 + p14 + p15 + p16 + p17 + p18 + p19  + p20 +
             p21 + p22 + p23 + p24 + p25 + p26 + p27 + p28,
-   afect = p1 + p2 + p11 + p14 + p15 + p21,
-   competence = p3 + p9 + p20 + p23 + p24 + p27,
-   value = p5 + p7 + p8 + p10 + p12 + p13 + p16 + p19 + p25,
-   difficulty = p4 + p6 + p17 + p18 + p22 + p26 + p28
+  ## Compute dimensions (means for comparison):
+   afect = (p1 + p2 + p11 + p14 + p15 + p21) / 6 ,
+   competence = (p3 + p9 + p20 + p23 + p24 + p27) / 6,
+   value = (p5 + p7 + p8 + p10 + p12 + p13 + p16 + p19 + p25) / 9,
+   difficulty = (p4 + p6 + p17 + p18 + p22 + p26 + p28) / 7
    ) %>%
   select (pws, sex, age, afect, competence, value, difficulty )
 ####
 ### s0 :: ready to analize
 
+## M/F structure
+sex.f <- s0 |> select (sex) |> group_by(sex) |>
+  summarize( n=n() ) |>
+  mutate(prop=n/sum(n) * 100 )
+
+sex.t <- kable(sex.f, col.names = c('płeć', 'n', '%'))
+sex.t
+
+## chart
+p.1 <- ggplot(sex.f, aes(x = reorder(sex, n), y = n )) +
+  ggtitle("Respondents by gender") +
+  xlab("") + ylab("%") +
+  ## position=dodge
+  geom_bar(position = 'dodge', stat = 'identity', fill = "steelblue") +
+  geom_text(data=sex.f, aes(label=sprintf("%.2f", prop), y= prop), hjust=1.5, color="white" ) +
+  coord_flip()
+p.1
+
+##
+## AtS by gender
+##
+ex.sex.f <- s0 |>
+  select (pws, sex) |>
+  group_by(sex) |>
+  summarize(m=mean(pws))
+
+ex.sex.t <- kable(ex.sex.f, col.names = c('płeć', 'średni pws'))
+ex.sex.t
+
+##
+## Chart
+##
+p.2 <- ggplot(ex.sex.f, aes(x = reorder(sex, pws), y = pws )) +
+  ggtitle("PwS wg płci") +
+  xlab("") + ylab("%") +
+  geom_bar(stat = 'identity', fill = "steelblue") +
+  geom_text(aes(label=sprintf("%.2f", pws), y= pws), hjust=1.5, color="white" ) +
+  coord_flip()
+
+p.2
+
+##
+## afect, competence, value, difficulty 
+##
+
+p.3 <- s0 |> select (afect, competence, value, difficulty) |>
+  pivot_longer(cols = everything(),
+    names_to = 'facet', values_to = 'value') |>
+  ggplot(aex(y=value, x=facet)) +
+  geom_boxplot() +
+  ggtitle('Affect, compenence, value and difficulty')
+
+p.3
+
+##
+## ats vs age
+##
+ggplot(s0, aes(x=age, y=pws)) +
+  geom_smooth(method='lm', se=FALSE) +
+  geom_point()
 
 
+## linear regression
 
+m1 <- lm(data=s0, pws ~ age )
+summary(m1)
+
+## End ##
